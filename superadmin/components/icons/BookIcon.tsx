5317f7fe10c9237ea41747adcdb2a4d9d74f7286
info.digitalmount <info.digitalmount@gmail.com>
2025-11-30 10:04:32 +0000

    Remove superadmin components and configuration

--- superadmin/components/icons/BookIcon.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const BookIcon = (props: React.SVGProps<SVGSVGElement>) => (
-    <svg {...props} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
-        <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-16.512A8.967 8.967 0 0118 3.75c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-16.512v16.5" />
-    </svg>
-);
--- superadmin/components/icons/GraduationCapIcon.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const GraduationCapIcon = (props: React.SVGProps<SVGSVGElement>) => (
-    <svg {...props} xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-        <path d="M21.71,6.29,12.71,2.29a2,2,0,0,0-1.42,0L2.29,6.29a2,2,0,0,0-1,1.72V14a2,2,0,0,0,1,1.72l8.29,4a2,2,0,0,0,1.42,0l8.29-4a2,2,0,0,0,1-1.72V8A2,2,0,0,0,21.71,6.29ZM4,13.24V8.76L11,12.25v4Zm15,0-7-3.49V8.25L19,5.76V13a1,1,0,0,0,.5.87l.18.09Z"/>
-    </svg>
-);
